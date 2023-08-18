d0b9d2ff27c52bcb1700647bf277ef3df1f81d9e
JUMTBOX <yjey12@naver.com>
2023-08-18 21:14:00 +0900

    ...

--- controller/boardController.js
+++ controller/boardController.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const mongoClient = require("./mongoConnect");
 
 const getAllArticle = async (req, res) => {
@@ -20,6 +21,7 @@ const createArticle = async (req, res) => {
     const board = client.db("board").collection("articles");
 
     const addArticle = await board.insertOne({
+      article_id: req.body.article_id,
       title: req.body.title,
       author: req.session.userID,
       content: req.body.content,
@@ -27,6 +29,18 @@ const createArticle = async (req, res) => {
     if (addArticle) res.status(200).json("포스팅 성공!");
   } catch (err) {
     res.status(400).json("포스팅 실패");
+    console.error(err);
+  }
+};
+
+const modifyArticle = async (req, res) => {
+  try {
+    const client = await mongoClient.connect();
+    const board = client.db("board").collection("articles");
+    const getModifiedOne = board.findOne({
+      article_id: ObjectId(req.params.id),
+    });
+  } catch (err) {
     console.error(err);
   }
 };
