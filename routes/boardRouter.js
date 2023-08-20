ec89cd2005403961f982c3dda2b663ee6b05e8fb
JUMTBOX <yjey12@naver.com>
2023-08-20 17:49:09 +0900

    ...

--- routes/boardRouter.js
+++ routes/boardRouter.js
@@ -3,9 +3,11 @@ const Router = express.Router();
 const {
   getAllArticle,
   createArticle,
+  getOneArticle,
 } = require("../controller/boardController");
 
 Router.get("/", getAllArticle);
+Router.get("/:id", getOneArticle);
 Router.post("/write", createArticle);
 
 module.exports = Router;
